c5af2ddb73b9afbc7c6deddb576ba35e6d51edba
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-04-02 19:18:50 -0700

    got process to automatically read spreadsheets into R started

--- read_pipe_data.R
+++ read_pipe_data.R
@@ -26,6 +26,77 @@ l_xlxs_fns <-
 l_xlxs_fns[1]
 # [1] "/home/jiml/HotWaterResearch/projects/How Low/Carl's data/12PEXBareRawData2.xlsx"
 
+# turn this into a data.table
+# to add metadata to before trying to read the test data itself
+DT_tests <- data.table(file=l_xlxs_fns)
+
+# just the file names
+DT_tests[, fname:= str_extract(file,"a/((.+).xlsx)")]
+DT_tests[, fname:= str_remove(fname, "a/")]
+
+
+# try readxl
+if(!require(readxl)){install.packages("readxl")}
+library(readxl)
+
+# this is going to be really ugly
+for(i in 1:nrow(DT_tests)) {
+  # read in the header info as a tibble
+  tb_header <- read_xlsx(path = DT_tests[i]$file,
+                      sheet = "Sheet1",
+                      range = "A1:G16",
+                      col_names = FALSE)
+  
+  # convert to data.frame to extract specific elements
+  DF_header <- as.data.frame(tb_header)
+
+  # put elements in DT_tests
+  DT_tests[i, `:=` (title    = DF_header[1,1],
+                    ODin     = DF_header[2,2],
+                    IDin     = DF_header[3,2],
+                    gal_pls  = DF_header[2,7], # gallons per pulse
+                    ft_gal   = DF_header[4,2])
+           ]
+  
+  # CPVC files
+  DT_tests[grep("CPVC",fname), 
+           `:=` (TC1_gal  = DF_header[7,3],
+                 TC2_gal  = DF_header[8,3],
+                 TC3_gal  = DF_header[9,3],
+                 TC4_gal  = DF_header[10,3],
+                 TC5_gal  = DF_header[11,3],
+                 TC6_gal  = DF_header[12,3],
+                 TC13_gal = DF_header[13,3],
+                 TC14_gal = DF_header[14,3],
+                 TC22_gal = DF_header[15,3],
+                 TC23_gal = DF_header[16,3])
+           ]
+
+  # non-CPVC files
+  DT_tests[grep("CPVC",fname, invert = TRUE), 
+           `:=` (TC1_gal  = DF_header[6,3],
+                 TC2_gal  = DF_header[7,3],
+                 TC3_gal  = DF_header[8,3],
+                 TC4_gal  = DF_header[9,3],
+                 TC5_gal  = DF_header[10,3],
+                 TC6_gal  = DF_header[11,3],
+                 TC13_gal = DF_header[12,3],
+                 TC14_gal = DF_header[13,3],
+                 TC22_gal = DF_header[14,3],
+                 TC23_gal = DF_header[15,3])
+           ]
+  
+}
+             
+ 
+
+str(DT_tests)
+DT_tests[,2:17]
+
+# save the header data as a csv file
+write.csv(DT_tests, file= paste0(wd_data,"DT_tests.csv"), row.names = FALSE)
+
+
 
 # put this in setup.R when it's working
 # try the tidyverse readxl
