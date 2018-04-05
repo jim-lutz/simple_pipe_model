37cd0e4e2f5053f8e3a4a3d0300dd84c710d8238
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-04-05 11:50:46 -0700

    first attempt to read all data

--- read_data.R
+++ read_data.R
@@ -1,6 +1,7 @@
 # read_data.R
 # script to read TC data values from 
 # /home/jiml/HotWaterResearch/projects/How Low/Carl's data/*.xlsx
+# saves uncleaned, unlabeled data.tables as *.xlsx.1.Rdata
 # Jim Lutz "Wed Apr  4 15:44:00 2018"
 
 # set packages & etc
@@ -15,34 +16,43 @@ wd_pipe_data <- "/home/jiml/HotWaterResearch/projects/How Low/Carl's data"
 # load the test info
 load(file = paste0(wd_data, "DT_test_info.Rdata"))
 
-# 
+# look at DT_test_info
+View(DT_test_info)
 
 # loop through all the file names
 # (this is going to be really ugly)
-# for(i in 1:nrow(DT_test_info)) {
+for(i in 1:nrow(DT_test_info)) {
   
-  # for now test on the first spreadsheet
-  i = 1
-  
+  # this is for testing on just the first spreadsheet
+  # i = 1
+
   # read in the data as a tibble
   tb_data <- read_xlsx(path = DT_test_info[i]$file,
                          sheet = "Sheet1",
-                         range = "A23:R65487", # hope got all data ever
+                         range = DT_test_info[i]$data_range, 
                          col_names = FALSE)
-  # number at top looks OK
-  pluck(tb_data, 7,2)
-  
-  pluck(tb_data, 1,2)
-  # date is a chr 
-  # [1] "40130.118900462963"
 
-  tail(tb_data)
-  pluck(tb_data,65462,2)
+  # read in the notes as a tibble
+  tb_notes <- read_xlsx(path = DT_test_info[i]$file,
+                       sheet = "Sheet1",
+                       range = DT_test_info[i]$notes_range, 
+                       col_names = FALSE)
   
-  nrow(tb_data)
-  
-  t <-   tail(tb_data)
+  # merge notes onto data
+  tb_data <- bind_cols(tb_data,tb_notes)
 
-  # seems to have read it all OK except for date  
-  
-# }
+  # convert to data.table
+  DT_data.1 <- as.data.table(tb_data)
+
+  # save data.table
+  save(DT_data.1, file = paste0(wd_data,DT_test_info[i]$fname,".1.Rdata"))
+
+  }
+# Error: Cell references aren't uniformly A1 or R1C1 format:
+# A24:R115758
+# In addition: There were 34 warnings (use warnings() to see them)
+# all but the last warning are about 'NAN' in column E
+# seems to have choked in 34CPVCBareRawData2.xlsx
+
+
+
