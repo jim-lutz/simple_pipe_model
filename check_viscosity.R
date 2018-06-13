600798a2a1a661142f3521ef429f535846eb4ed1
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-06-13 15:44:05 -0700

    solved problems with absolute vs gauge pressure, was getting steam at lowest pressure and higher temps

--- check_viscosity.R
+++ check_viscosity.R
@@ -58,7 +58,7 @@ ggplot(data=DT_miniREFPROP[!is.na(T)], aes(x=T, y= μ, color=as.factor(Pnom)))+
 # look at viscosity vs pressure by (some) temperatures
 ggplot(data=DT_miniREFPROP[T %in% seq(40,180,20)], aes(x=P, y= μ, color=as.factor(T)))+
   geom_line()
-# it's very flat, probably can ignore pressure impacts on viscosity
+# it's very flat, can ignore pressure impacts on viscosity
 
 # look at density vs temp by pressure
 ggplot(data=DT_miniREFPROP[!is.na(T)], aes(x=T, y= D, color=as.factor(Pnom)))+
@@ -76,8 +76,10 @@ DT_CHNOSZ[,dummy:=NULL]
 # add temperatures in Kelvin
 DT_CHNOSZ[ , T.K := (T-32)/1.8 + 273.15]
 
-# add pressures in bar from PSIG 
-DT_CHNOSZ[ , P.bar := ( P * 6894.757 + 	10132.5 ) / 100000 ]
+# add pressures in bar 
+# from PSI gauge + 14.69595 to convert to PSI absolute     
+# * Pa/PSI  /  Pa/bar
+DT_CHNOSZ[ , P.bar := ( (P + 14.69595) * 6894.757 ) / 100000 ]
 
 # now calc viscosity (g cm^-1 s^-1 = 0.1 * pascal second (Pa · s))
 DT_CHNOSZ[, mu.poise := water.SUPCRT92(property='visc', T=T.K, P=P.bar )]
@@ -89,8 +91,17 @@ DT_CHNOSZ[, mu.lbm_ft_s := mu.poise / 10 / 1.488164 ]
 # add density 
 DT_CHNOSZ[, rho.kg_m_3 := water.SUPCRT92(property='rho', T=T.K, P=P.bar )]
 
-# convert density from kg/m^3 to lbm/ft^3
-DT_CHNOSZ[, rho.lbm_ft_3 := rho.kg_m_3 * 16.01846]
+# convert density from kg/m^3 (* kg/lbm * m^3/in^3 * 12in*12in*12in)  to lbm/ft^3 
+DT_CHNOSZ[, rho.lbm_ft_3 := rho.kg_m_3 / 4.5359237E-01 * 1.6387064E-05 * (12*12*12)]
+
+DT_CHNOSZ[,list( rho.kg_m_3, rho.lbm_ft_3)]
+qplot(data = DT_CHNOSZ, rho.kg_m_3, rho.lbm_ft_3 )
+# looks like it should now
+
+qplot(data = DT_CHNOSZ, rho.kg_m_3 )
+DT_CHNOSZ[rho.kg_m_3<100]
+DT_CHNOSZ[P==0 & T>100]
+
 
 # merge & plot
 names(DT_CHNOSZ)
@@ -122,16 +133,31 @@ ggplot(data=DT_water[P==75], aes(x=T, y= (μ - mu.lbm_ft_s)) )+
 # CHNOSZ values about .0000003 too high
 
 # compare density
-DT_water[,list(D, rho.lbm_ft_3)]
-# bad units conversion problem?
+DT_water[,list(D, rho.kg_m_3, rho.lbm_ft_3)]
+DT_water[,list(D, rho.lbm_ft_3, D-rho.lbm_ft_3)]
+# CHNOSZ about 0.001 lbm/ft^3 too high
 
-
-
+# look at density vs temp at one pressure 
+ggplot(data=DT_water[P==75], aes(x=T, y= D) )+
+  geom_line() +
+  geom_line( aes(x=T, y=rho.lbm_ft_3), color='red')
+# can't tell them apart
 
 # look at difference in density as percent of miniREFPROP values
-ggplot(data=DT_water[P==75], aes(x=T, y= (D - rho.lbm_ft_3)/D) )+
+ggplot(data=DT_water, 
+       aes(x=T, y= (D - rho.lbm_ft_3), color=as.factor(Pnom)) )+
   geom_line() +
-  scale_y_continuous(name = "difference in density"
-  )
+  scale_y_continuous(name = "difference in density (miniREFPROP - CHNOsZ)")
+# not perfect, but not too bad, .001 out ~ 60
+
+# compare density miniREFPROP & CHNOSZ by Pnom
+DT_water[,list(diff.rho     = mean(D-rho.lbm_ft_3),
+               max.diff.rho = max(D-rho.lbm_ft_3),
+               min.diff.rho = min(D-rho.lbm_ft_3)),
+         by=Pnom]
+
+
+# calculate and compare kinematic viscosity
+
 
 
